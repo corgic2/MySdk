a00dcd9f151af89dc32238d939f6d33b62efc2fd
liaofeng <liaofeng@manteiatech.com>
2025-01-22 15:36:04 +0800

    feat:JsonObject接口初步构造

--- JsonObject/JsonObject.h
+++ JsonObject/JsonObject.h
@@ -6,15 +6,51 @@
 // </remarks>
 // ************************************************************
 #include <string>
+#include <unordered_map>
+#include "FileSystem.h"
+#if 0
+// 用于存放JsonObject对象
+class JsonObjectPrivate
+{
+public:
+    enum EM_JsonValue
+    {
+        Object,
+        Array,
+        String,
+        Boolean,
+        Null,
+        Number
+    };
+    JsonObjectPrivate();
+    ~JsonObjectPrivate();
+    //从index开始以后的内容转化为Object,Array,Value等
+    JsonObjectPrivate ParseObject(const std::string& content,size_t &index);
+    JsonObjectPrivate ParseArray(const std::string& content,size_t &index);
+    JsonObjectPrivate ParseValue(const std::string& content,size_t &index);
 
+    // SetAndGet对象类型
+    void SetValueType(const EM_JsonValue &enumValue);
+    void GetValueType();
+
+private:
+    std::unordered_map<std::string, JsonObjectPrivate> m_JsonObject;
+    EM_JsonValue m_valueType = EM_JsonValue::Object;
+};
 
 class JsonObject
 {
 public:
     JsonObject();
+    JsonObject(const std::string &filePath); // 从文件中读取转化Json对象
     ~JsonObject();
 
+    JsonObject &operator=(const JsonObject &obj);
+    // SetAndGet 实例对象
+    void SetJsonObject(const JsonObject &obj);
+    JsonObjectPrivate& GetJsonObject();
 private:
-
+    std::string m_content;
+    JsonObjectPrivate m_obj;
 };
-
+#endif 
