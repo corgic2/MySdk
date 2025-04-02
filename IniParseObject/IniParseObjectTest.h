8bc6e714a62d4602325e58877a26735b1a58014a
brs.lf <876338266@qq.com>
2025-04-02 21:45:47 +0800

    feat:INI解析

---
+++ IniParseObject/IniParseObjectTest.h
@@ -0,0 +1,9 @@
+﻿#pragma once
+#include "INIParseObject.h"
+
+void InitParseObjectTest()
+{
+    my_sdk::INIParseObject obj;
+    obj.Load("../TestFiles/testIni.ini");
+    obj.PrintAll();
+}
--- main.cpp
+++ main.cpp
@@ -4,11 +4,12 @@
 #include "JsonObject/JsonObjectTest.h"
 #include "DesignPattern/SinglePattern.h"
 #include "SDKCommonDefine/SDKUtils.h"
-
+#include "IniParseObject/IniParseObjectTest.h"
 int main()
 {
     //FileSystemTest();
-    JsonObjectTest();
+    //JsonObjectTest();
+    InitParseObjectTest();
     //std::string value = my_utils::MyUtils::getinstance()->AddBigInterNumber("12312453152344523452346234623452", "43252346234985798465238497");
     //std::cout << value << std::endl;
     //SinglePatternClass::getinstance()->print();0
