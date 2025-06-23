5bbc42b55b8901f8ecaed2151947b930096f9e1d
brs.lf <876338266@qq.com>
2025-06-23 11:03:54 +0800

    feat: 删除不必要的代码

--- IniParseObject/IniParseObjectTest.h
+++
@@ -1,9 +0,0 @@
-﻿#pragma once
-#include "INIParseObject.h"
-//
-//void InitParseObjectTest()
-//{
-//    my_sdk::INIParseObject obj;
-//    obj.Load("../TestFiles/testIni.ini");
-//    obj.PrintAll();
-//}
